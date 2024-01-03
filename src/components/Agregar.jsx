e279b0d765c89d08f58551f72eb783faf94c5927
mpluisacarolina <mpluisacarolina@gmail.com>
2024-01-03 14:05:56 -0400

    cambios en Agregar

--- src/components/Agregar.jsx
+++ src/components/Agregar.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export const Agregar = () => {
+  const titulo = "Añadir Película";
   return (
     <div className="add">
-                <h3 className="title">Añadir Película</h3>
-                <form>
-                    <input type="text" placeholder="Título" />
-                    <textarea placeholder="Descripción"></textarea>
-                    <input type="submit" value="Guardar" />
-                </form>
-            </div>
+      <h3 className="title">Añadir Película</h3>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Descripción"></textarea>
+        <input type="submit" value="Guardar" />
+      </form>
+    </div>
   )
 }
