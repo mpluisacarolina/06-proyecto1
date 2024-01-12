2a435882ea5fd355084c5a8c826c4a23215a8532
mpluisacarolina <mpluisacarolina@gmail.com>
2024-01-12 17:11:47 -0400

    Mejoras al buscador. Mensaje cuando no se encuentra una película.

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 
-export const Buscador = ({listadoState, setListadoState}) => {
+export const Buscador = ({ listadoState, setListadoState }) => {
 
-    const [busqueda, setBusqueda] = useState('')
+    const [busqueda, setBusqueda] = useState('');
+    const [noEncontrado, setNoEncontrado] = useState(false);
 
     const buscarPeli = (e) => {
         //Crear el estado y actualizarlo
@@ -13,8 +14,11 @@ export const Buscador = ({listadoState, setListadoState}) => {
             return peli.titulo.toLowerCase().includes(busqueda.toLocaleLowerCase());
         });
 
-        if(busqueda.length <= 1 || pelis_encontradas.length <= 0){
+        if (busqueda.length <= 1 || pelis_encontradas.length <= 0) {
             pelis_encontradas = JSON.parse(localStorage.getItem("pelis"));
+            setNoEncontrado(true);
+        } else {
+            setNoEncontrado(false);
         }
 
         //Actualizar estado del listado principal con lo que se ha filtrado
@@ -22,19 +26,22 @@ export const Buscador = ({listadoState, setListadoState}) => {
 
     }
 
-return (
-    <div className="search">
-        <h3 className="title"> Buscador: {busqueda}</h3>
-        <form>
-            <input type="text"
-                id="search_field"
-                name="busqueda"
-                onChange={buscarPeli}
-                placeholder="Buscar"
-                autoComplete="off"
-            />
-            <button>Buscar</button>
-        </form>
-    </div>
-)
+    return (
+        <div className="search">
+            <h3 className="title"> Buscador: {busqueda}</h3>
+            {(noEncontrado == true && busqueda.length > 1) && (
+                <span className="no-encontrado"> No se ha encontrado ninguna coincidencia</span>
+            )}
+            <form>
+                <input type="text"
+                    id="search_field"
+                    name="busqueda"
+                    onChange={buscarPeli}
+                    placeholder="Buscar"
+                    autoComplete="off"
+                />
+                <button>Buscar</button>
+            </form>
+        </div>
+    )
 }
