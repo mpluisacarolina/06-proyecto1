aafa9745096c5af5e9dbbbdb29015be8a650a699
mpluisacarolina <mpluisacarolina@gmail.com>
2024-01-12 13:38:25 -0400

    Función Borrar

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -11,13 +11,27 @@ export const Listado = ({ listadoState, setListadoState }) => {
         conseguirPeliculas();
     }, []);
 
-
     const conseguirPeliculas = () => {
         let peliculas = JSON.parse(localStorage.getItem("pelis"));
 
         setListadoState(peliculas);
 
         return peliculas;
+    }
+
+    const borrarPeli = (id) => {
+        //Conseguir películas almacenadas
+        let pelis_almacenadas = conseguirPeliculas();
+
+        //Filtrar las películas para que elimine del array la que no quiero
+        let nuevo_array_pelis = pelis_almacenadas.filter(peli => peli.id !== parseInt(id));
+
+        //Actualizar el estado del listado
+        setListadoState(nuevo_array_pelis);
+
+        //Actualizar datos en el localStorage
+        localStorage.setItem('pelis', JSON.stringify(nuevo_array_pelis));
+        
     }
 
     return (
@@ -28,8 +42,8 @@ export const Listado = ({ listadoState, setListadoState }) => {
                         <article key={peli.id} className="peli-item">
                             <h3 className="title">{peli.titulo}</h3>
                             <p className="descripcion">{peli.descripcion}</p>
-                            <button className="edit" onClick={() => (setEditar(peli.id))}>Editar</button>
-                            <button className="delete">Borrar</button>
+                            <button className="edit" onClick={() => (setEditar(peli.id))} >Editar</button>
+                            <button className="delete" onClick={() => borrarPeli(peli.id)}>Borrar</button>
 
                             {/*Aparece formulario de editar*/}
                             {editar === peli.id && (
@@ -39,7 +53,6 @@ export const Listado = ({ listadoState, setListadoState }) => {
                                     setListadoState={setListadoState}
                                 />
                             )}
-
                         </article>
                     )
                 })
